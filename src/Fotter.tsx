6176189b90c50070f5c53274c58bbb2dbe8f362b
Jetsada Gujan <jamanggaza@gmail.com>
2024-07-17 11:05:12 +0700

    setup project

---
+++ src/Fotter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Fotter() {
+  return (
+    <div style={{ width: "100%", height: "50px", backgroundColor: "green" }}>
+      Foot
+    </div>
+  );
+}
